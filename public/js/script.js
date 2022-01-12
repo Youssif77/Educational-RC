b816ef79d197e649d76e5d21e9ff866d3dbaad5e
youssif hany <youssifhany75@gmail.com>
2022-01-13 01:14:02 +0200

    add help-model

--- public/js/script.js
+++ public/js/script.js
@@ -1,65 +1,129 @@
 'use strict';
+import { getCircle, addClass, removeClass } from './helper.js';
 
 const loader = document.querySelector('.loader');
-const appContainer = document.querySelector('.container');
-const questionsMatchCircles = document.querySelectorAll('[data-question]');
-const answersMatchCircles = document.querySelectorAll('[data-answer]');
+const questionsSelectors = document.querySelectorAll('[data-question]');
+const answersSelectors = document.querySelectorAll('[data-answer]');
+
 const correctSound = document.querySelector('.correct-sound');
 const incorrectSound = document.querySelector('.incorrect-sound');
 
+const appContainer = document.querySelector('.container');
+const backdropContainer = document.querySelector('.backdrop');
+const resourceContainer = document.querySelector('.resource');
+const helpContainer = document.querySelector('.help');
+const gameContainer = document.querySelector('.game');
+const linesContainer = document.querySelector('.game__results svg');
+
+const replayBtn = document.querySelector('.replay');
+const showAnsBtn = document.querySelector('.show-ans');
+const resourceBtn = document.querySelector('.resource-btn');
+const helpBtn = document.querySelector('.help-btn');
+const closeBtns = document.querySelectorAll('.close-btn');
+
 const state = {
   currentQuestion: '',
-  currentQuestionCircle: null,
+  currentQuestionSelector: null,
 };
 
+// Event listeners
 document.addEventListener('DOMContentLoaded', function hideLoader() {
-  console.log('START PROGRAM');
-  loader.classList.add('hidden');
+  addClass(loader, 'hidden');
   resclaeWindow();
 });
 
 window.addEventListener('resize', resclaeWindow);
 
-questionsMatchCircles.forEach(circle => {
+questionsSelectors.forEach(circle => {
   circle.addEventListener('click', selectQuestion);
 });
 
-answersMatchCircles.forEach(circle => {
+answersSelectors.forEach(circle => {
   circle.addEventListener('click', selectAnswer);
 });
 
+showAnsBtn.addEventListener('click', showAnswer);
+
+replayBtn.addEventListener('click', resetGame);
+
+resourceBtn.addEventListener('click', openResourcModal);
+
+helpBtn.addEventListener('click', openHelpModal);
+
+closeBtns.forEach(btn => btn.addEventListener('click', closeHelpModel));
+
+// Event Handlers
 function selectQuestion(e) {
-  const currentQuestionCircle = e.target;
+  const currentQuestionSelector = e.target.closest('[data-question]');
 
-  if (currentQuestionCircle.classList.contains('disabled')) return;
+  if (currentQuestionSelector.classList.contains('disabled')) return;
 
-  questionsMatchCircles.forEach(circle => circle.classList.remove('active'));
-  currentQuestionCircle.classList.add('active');
+  questionsSelectors.forEach(selector =>
+    removeClass(getCircle(selector), 'active')
+  );
 
-  state.currentQuestion = currentQuestionCircle.dataset.question;
-  state.currentQuestionCircle = currentQuestionCircle;
-  console.log(state);
+  addClass(getCircle(currentQuestionSelector), 'active');
+
+  state.currentQuestion = currentQuestionSelector.dataset.question;
+  state.currentQuestionSelector = currentQuestionSelector;
 }
 
 function selectAnswer(e) {
-  const currentAnswerCircle = e.target;
+  const currentAnswerSelector = e.target.closest('[data-answer]');
 
   if (!state.currentQuestion) return;
-  if (currentAnswerCircle.classList.contains('disabled')) return;
+  if (currentAnswerSelector.classList.contains('disabled')) return;
 
-  if (state.currentQuestion == currentAnswerCircle.dataset.answer) {
-    state.currentQuestionCircle.classList.add('disabled');
-    state.currentQuestionCircle.classList.remove('active');
-    state.currentQuestion = '';
-    state.currentQuestionCircle = null;
-    currentAnswerCircle.classList.add('disabled');
-    console.log('CORRECT');
-    correctSound.play();
-    return;
+  // Show wrong answer img and play incorrect sound
+  if (state.currentQuestion !== currentAnswerSelector.dataset.answer) {
+    const crossImg = currentAnswerSelector.querySelector('.wrong-answer');
+    removeClass(crossImg, 'hidden');
+
+    setTimeout(() => {
+      addClass(crossImg, 'hidden');
+    }, 500);
+
+    return incorrectSound.play();
   }
 
-  incorrectSound.play();
-  console.log(currentAnswerCircle.dataset);
+  // Render line between matched question and answer
+  renderConnectionLines(
+    getCircle(state.currentQuestionSelector),
+    getCircle(currentAnswerSelector)
+  );
+
+  // Disable those question and answer
+  disableQuestionAndAnswer(
+    state.currentQuestionSelector,
+    currentAnswerSelector
+  );
+
+  // Unselect the current question circle
+  removeClass(getCircle(state.currentQuestionSelector), 'active');
+
+  // Rest state
+  state.currentQuestion = '';
+  state.currentQuestionSelector = null;
+  correctSound.play();
+}
+
+function disableQuestionAndAnswer(questionSelector, answerSelector) {
+  addClass(questionSelector.querySelector('img'), 'disabled');
+  addClass(getCircle(questionSelector), 'selected');
+
+  addClass(answerSelector.querySelector('h2'), 'disabled');
+  addClass(getCircle(answerSelector), 'selected');
+}
+
+function renderConnectionLines(questionCircle, answerCircle) {
+  const x1 = questionCircle.offsetLeft + questionCircle.offsetWidth / 2;
+  const x2 = answerCircle.offsetLeft + answerCircle.offsetWidth / 2;
+
+  // X1 from left the quesion, y1 from the top of svg
+  // X2 from left the answer, y2 from the bottom of svg
+  const html = `<line  x1="${x1}" y1="0" x2="${x2}" y2="120"  />`;
+
+  linesContainer.insertAdjacentHTML('beforeend', html);
 }
 
 function resclaeWindow() {
@@ -67,6 +131,7 @@ function resclaeWindow() {
   if (diffAppFromWindow > 0) {
     appContainer.style.right = `${diffAppFromWindow / 2}px`;
     appContainer.style.left = `${diffAppFromWindow / 2}px`;
+    // Reset the scale
     appContainer.style.transform = 'scale(1)';
     return;
   }
@@ -75,4 +140,70 @@ function resclaeWindow() {
   appContainer.style.transform = `scale(${
     window.innerWidth / appContainer.clientWidth
   })`;
+}
+
+function showAnswer() {
+  questionsSelectors.forEach(questionSelector => {
+    answersSelectors.forEach(answerSelector => {
+      if (questionSelector.dataset.question === answerSelector.dataset.answer) {
+        renderConnectionLines(
+          getCircle(questionSelector),
+          getCircle(answerSelector)
+        );
+      }
+
+      disableQuestionAndAnswer(questionSelector, answerSelector);
+    });
+  });
+
+  addClass(showAnsBtn, 'disabled');
+  showAnsBtn.removeEventListener('click', showAnswer);
+}
+
+function resetGame() {
+  // Reset the state
+  state.currentQuestion = '';
+  state.currentQuestionSelector = null;
+
+  // Reset the answers selectors
+  answersSelectors.forEach(answerSelector => {
+    removeClass(answerSelector.querySelector('h2'), 'disabled');
+    removeClass(getCircle(answerSelector), 'selected');
+  });
+
+  // Reset  the Qustions selectors
+  questionsSelectors.forEach(questionSelector => {
+    removeClass(questionSelector.querySelector('img'), 'disabled');
+    removeClass(getCircle(questionSelector), 'selected');
+  });
+
+  // Clear the lines between questions and answers
+  linesContainer.innerHTML = '';
+
+  // Reset show answer button
+  removeClass(showAnsBtn, 'disabled');
+  showAnsBtn.removeEventListener('click', showAnswer);
+  showAnsBtn.addEventListener('click', showAnswer);
+}
+
+function openResourcModal() {
+  showBackdrop();
+  removeClass(resourceContainer, 'hidden');
+}
+
+function openHelpModal() {
+  showBackdrop();
+  removeClass(helpContainer, 'hidden');
+}
+
+function showBackdrop() {
+  removeClass(backdropContainer, 'hidden');
+  addClass(gameContainer, 'hidden');
+}
+
+function closeHelpModel() {
+  addClass(helpContainer, 'hidden');
+  addClass(resourceContainer, 'hidden');
+  addClass(backdropContainer, 'hidden');
+  removeClass(gameContainer, 'hidden');
 }
